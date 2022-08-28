2620ad1f1d095155e3267a3c1ff3800033d406f4
Peter Popov <p.dim.popov@gmail.com>
2022-08-28 14:09:30 +0300

    parse --source-type option

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,20 @@
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum SourceType {
-    Raw, File,
+    Raw,
+    File,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+impl SourceType {
+    fn from(x: &str) -> Option<SourceType> {
+        match x {
+            "raw" => Some(SourceType::Raw),
+            "file" => Some(SourceType::File),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Config {
     pub source: String,
     pub source_type: SourceType,
@@ -15,12 +24,29 @@ impl Config {
     pub fn parse(mut input: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         input.next();
 
-        let src = match input.next() {
+        let source = match input.next() {
             Some(a) => a,
             _ => return Err("No source given"),
         };
 
-        Ok(Config {source: src.to_string(), source_type: SourceType::File})
+        let source_type = match input.next() {
+            Some(x) => match x.split_once("=") {
+                Some((key, val)) => match key {
+                    "--source-type" => match SourceType::from(val) {
+                        Some(st) => st,
+                        _ => return Err("Unknown source type"),
+                    },
+                    _ => return Err("Unknown option")
+                },
+                None => return Err("Invalid option"),
+            },
+            _ => SourceType::File,
+        };
+
+        Ok(Config {
+            source,
+            source_type,
+        })
     }
 }
 
@@ -31,9 +57,31 @@ mod config_tests {
     #[test]
     fn it_should_parse_minimal_variant_as_file() {
         expect_input_to_be_parsed_correctly(
-            "bf hello-world.bf", 
-            Config { source: "hello-world.bf".to_string(), source_type: SourceType::File }
+            "bf hello-world.bf",
+            Config {
+                source: "hello-world.bf".to_string(),
+                source_type: SourceType::File,
+            },
         )
+    }
+
+    #[test]
+    fn it_should_parse_with_source_type_flag() {
+        expect_input_to_be_parsed_correctly(
+            "bf [.] --source-type=raw",
+            Config {
+                source: "[.]".to_string(),
+                source_type: SourceType::Raw,
+            },
+        );
+
+        expect_input_to_be_parsed_correctly(
+            "bf hello.bf --source-type=file",
+            Config {
+                source: "hello.bf".to_string(),
+                source_type: SourceType::File,
+            },
+        );
     }
 
     fn expect_input_to_be_parsed_correctly(input: &'static str, expected: Config) {
@@ -41,6 +89,6 @@ mod config_tests {
 
         let result = Config::parse(input).unwrap();
 
-        assert_eq!(result, expected)        
+        assert_eq!(result, expected)
     }
 }
